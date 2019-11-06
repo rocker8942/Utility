9b1476de8c44a6151eeda29b50fb4d059306637e
joe park <rocker8942@gmail.com>
2019-11-06 22:12:38 +0800

    Add async method in Retry logic

---
+++ UtilityTests/RetryAsyncTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utility;
+
+namespace UtilityTests
+{
+    [TestClass]
+    public class RetryAsyncTest
+    {
+        private int _retries;
+
+        private async Task<bool> SucceedAlways(int anything)
+        {
+            _retries++;
+
+            return await Task.FromResult(true);
+        }
+
+        private async Task<bool> SuccessOnSubsequentTry(int anything)
+        {
+            _retries++;
+            if (_retries == 1)
+                throw new Exception("first try always fail");
+
+            return await Task.FromResult(true);
+        }
+
+        private async Task<bool> FailAlways()
+        {
+            _retries++;
+
+            throw new InvalidOperationException("first try always fail");
+        }
+
+        [TestMethod]
+        public async Task RetrySucceedsOnFirstTryAsync()
+        {
+            _retries = 0;
+
+            var completedDelegate = await Retry.DoAsync(
+                async () => await SucceedAlways(1)
+                , TimeSpan.FromSeconds(1));
+
+            Debug.WriteLine(_retries.ToString());
+            Assert.IsTrue(_retries == 1);
+        }
+
+        [TestMethod]
+        public async Task RetrySucceedsOnSubsequentTryAsync()
+        {
+            _retries = 0;
+            var completedDelegate = await Retry.DoAsync(
+                async () => await SuccessOnSubsequentTry(1)
+                , TimeSpan.FromSeconds(1));
+
+            Debug.WriteLine(_retries.ToString());
+            Assert.IsTrue(_retries > 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AggregateException))]
+        public async Task RetryFail()
+        {
+            _retries = 0;
+            bool completedDelegate = await Retry.DoAsync(
+                async () => await FailAlways()
+                , TimeSpan.FromSeconds(1)
+                , 3);
+
+            Debug.WriteLine(_retries.ToString());
+            Assert.IsTrue(_retries == 3);
+        }
+    }
+}
